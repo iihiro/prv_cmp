dbea14fa98affbec724cca72462925c5892d5065
Hiromichi Iizuka <iizuka@eduam.co.jp>
2020-02-25 10:35:26 +0900

    Add config file option

--- demo.sh
+++ demo.sh
@@ -12,7 +12,7 @@ done
 
 CONFFILE=${TOPDIR}/demo/dec/config.txt
 
-xterm -T "Dec"  -e "/bin/bash -c 'cd ${BINDIR}/dec    && ./dec ${KEYOPT}; exec /bin/bash -i'"&
-xterm -T "EncA" -e "/bin/bash -c 'cd ${BINDIR}/enc    && ./enc -t 0 10;   exec /bin/bash -i'"&
-xterm -T "EncB" -e "/bin/bash -c 'cd ${BINDIR}/enc    && ./enc -t 1  6;   exec /bin/bash -i'"&
-xterm -T "Eval" -e "/bin/bash -c 'cd ${BINDIR}/eval   && ./eval;          exec /bin/bash -i'"&
+xterm -T "Dec"  -e "/bin/bash -c 'cd ${BINDIR}/dec    && ./dec ${KEYOPT} -t ${CONFFILE}; exec /bin/bash -i'"&
+xterm -T "EncA" -e "/bin/bash -c 'cd ${BINDIR}/enc    && ./enc -t 0 10;                  exec /bin/bash -i'"&
+xterm -T "EncB" -e "/bin/bash -c 'cd ${BINDIR}/enc    && ./enc -t 1  6;                  exec /bin/bash -i'"&
+xterm -T "Eval" -e "/bin/bash -c 'cd ${BINDIR}/eval   && ./eval;                         exec /bin/bash -i'"&
